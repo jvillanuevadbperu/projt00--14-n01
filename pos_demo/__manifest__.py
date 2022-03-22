a5c425b682bbe9f94b5440ec1e4c067d6f9d7d80
jvillanuevadbperu <jvillanueva@dbperu.com>
2022-03-22 22:35:37 +0000

    [ADD] Basic Module - I02

--- pos_demo/__manifest__.py
+++ pos_demo/__manifest__.py
@@ -4,11 +4,14 @@
     'summary': "Point of sale demo module",
     'description': """Long description""",
     'author': "Parth Gajjar",
-    'website': "http://www.example.com",
+    'website': "https://www.odoo.com",
     'category': 'Point of Sale',
     'version': '14.0.1',
     'depends': ['point_of_sale'],
     'data': [
         'views/pos_assets.xml'
     ],
+    'qweb': [
+        'static/src/xml/pos_demo.xml'
+    ]
 }
